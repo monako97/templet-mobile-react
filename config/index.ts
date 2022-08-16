2e12c4b3c50b5d2981085e90a78326253c15ed15
moneko <poi.nyaa@qq.com>
2022-08-16 21:32:49 +0800

    1

--- config/index.ts
+++ config/index.ts
@@ -2,8 +2,30 @@ import type { PartialConfigType } from 'plugin-runtime';
 
 const conf: PartialConfigType = {
   designSize: 375,
-  gzip: false,
-  bundleAnalyzer: false
+  minifier: {
+    type: 'terser',
+  },
+  importOnDemand: {
+    antd: ['[source]/es/[name:-]', '[source]/es/[name:-]/style'],
+    lodash: '[source]/[name]',
+    '@ant-design/icons': {
+        transform: ({ name, source }) => {
+            if (name === 'createFromIconfontCN') {
+                return `${source}/es/components/IconFont`;
+            }
+            return `${source}/es/icons/${name}`;
+        },
+    },
+  },
+  proxy: [
+    {
+      context: ['/api/'],
+      target: 'http://127.0.0.1:8001/',
+      changeOrigin: true,
+      pathRewrite: { '^/api/': '/' },
+      secure: false,
+    }
+  ] as unknown as PartialConfigType['proxy'],
 };
 
 export default conf;
