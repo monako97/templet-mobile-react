d0247f770e0940bd2318d28b3b36b4a6119d626e
moneko <poi.nyaa@qq.com>
2022-08-29 20:51:14 +0800

    添加model用例
    添加model用例

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
-import { useLocale, useOutlet, useSelector, shallowEqual } from 'plugin-runtime';
+import React, { useEffect } from 'react';
+import { useLocale, useOutlet, useSelector, shallowEqual, useDispatch } from 'plugin-runtime';
 import styles from './index.less';
 import type { UserModelType } from '@/models/user';
 
 const Home: React.FC = () => {
   const outlet = useOutlet();
+  const dispatch = useDispatch();
   const { getLanguage } = useLocale();
   const userInfo = useSelector((state: { user: UserModelType }) => state.user, shallowEqual);
 
+  useEffect(() => {
+    dispatch({
+      type: 'user/fetchLoginByUserName',
+      payload: {
+        data: {
+          username: 'admin',
+          password: 'adminpsw',
+        },
+      },
+    });
+  }, [dispatch]);
   return (
     <React.Fragment>
       <div className={styles.details}>
